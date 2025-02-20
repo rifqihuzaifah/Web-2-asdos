0ed75b4f4200ae3588a17917a9be1c26b42d82a3
rifqihuzaifah <rifqihuzaifah8@gmail.com>
2025-02-20 12:14:16 +0700

    praktikum 1

--- README.md
+++
@@ -1,6 +0,0 @@
-# pemograman web 2
-## repositori praktikum pemrograman web 2
-
-- NAMA : rifqi huzaifah
-- NIM : 0110124154
-- ROMBEL : SI11
---
+++ variable_user.php
@@ -0,0 +1,14 @@
+<?php
+// mendefinisikan variable
+$nama = 'rifqi';
+$umur = 18;
+$berat = 47.11;
+
+echo 'nama saya ' . $nama;
+echo '<br/>umur : ' . $umur . ' tahun';
+echo '<br/>berat : ' . $berat. ' kg'
+
+
+
+
+?>
